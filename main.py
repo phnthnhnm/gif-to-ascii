3c8e90b2084464a23ddb34203970f5dbf55f68ff
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-10 09:50:57 +0700

    fix(cli): --output-path now works again

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def main():
     converter = GIFToASCIIConverter(args.gif_file)
     ascii_frames = converter.convert_to_ascii()
     converter.display_ascii_animation(ascii_frames)
+    
+    with open(args.output, 'w') as output_file:
+        for frame in ascii_frames:
+            output_file.write(frame + '\n\n')
 
 
 if __name__ == "__main__":
